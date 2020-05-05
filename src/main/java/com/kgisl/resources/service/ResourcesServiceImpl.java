ae04f89cc2cfe7144c7b6926366d9c686c9243b6
Ramyabalraj <57798910+Ramyabalraj@users.noreply.github.com>
2020-05-05 16:06:41 +0530

    Update ResourcesServiceImpl.java

--- src/main/java/com/kgisl/resources/service/ResourcesServiceImpl.java
+++ src/main/java/com/kgisl/resources/service/ResourcesServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 import javax.transaction.Transactional;
 
-import com.kgisl.resources.entity.Resources;
+import com.kgisl.resources.entity.*;
 import com.kgisl.resources.repository.ResourcesRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,23 +36,39 @@ public class ResourcesServiceImpl implements ResourcesService {
     String resourcesName=resources.getResName();
     System.out.println(resourcesName);
     Boolean b=resourcesRepository.existsByresName(resourcesName);
+    System.out.println(b);
     if(resourcesName == null){
     System.out.println("null resources");
-    }
-    else{
-     if(b==false ){
-     Resources r=new Resources(0,"Java Script 1","Java Script","Video","LMS.com");
-     Resources r2=new Resources(0,"Java 1","Java","Video","LMS.com");
-     Resources r3=new Resources(0,"Angular 1","Angular","Video","LMS.com");
-     Resources r4=new Resources(0,"Spring Boot 1","Spring Boot","Video","LMS.com");
+      Level l=new Level(1,"Beginner");
+      Level l2=new Level(2,"Intermediate");
+      Level l3=new Level(3,"Professional");
+     Resources r=new Resources(0,"Java Script 1","Java Script","Video","LMS.com",l);
+     Resources r2=new Resources(0,"Java 1","Java","Video","LMS.com",l);
+     Resources r3=new Resources(0,"Angular 1","Angular","Video","LMS.com",l);
+     Resources r4=new Resources(0,"Spring Boot 3","Spring Boot","Video","LMS.com",l);
+     Resources r5=new Resources(0,"Java Script 2","Java Script","Video","LMS.com",l2);
+     Resources r6=new Resources(0,"Java 2","Java","Video","LMS.com",l2);
+     Resources r7=new Resources(0,"Angular 3","Angular","Video","LMS.com",l3);
+     Resources r8=new Resources(0,"Spring Boot 3","Spring Boot","Video","LMS.com",l3);
      resourcesRepository.save(r);
      resourcesRepository.save(r2);
      resourcesRepository.save(r3);
      resourcesRepository.save(r4);
-     System.out.println("resources"+resources.getResId());
+     resourcesRepository.save(r5);
+     resourcesRepository.save(r6);
+     resourcesRepository.save(r7);
+     resourcesRepository.save(r8);
+
+    
      List<Resources> list=  get();
      return list;
-      }
+
+    }
+    else{
+
+     if(b==false ){
+        System.out.println("resources  is Already there...");
+            }
      else{
      System.out.println("resource is Already there...");
      }
