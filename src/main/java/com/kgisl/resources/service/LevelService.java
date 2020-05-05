e7bc65b4e6ede81660084dca986c0ba156ecb5a3
Ramyabalraj <57798910+Ramyabalraj@users.noreply.github.com>
2020-05-05 16:00:00 +0530

    Create LevelService.java

---
+++ src/main/java/com/kgisl/resources/service/LevelService.java
@@ -0,0 +1,15 @@
+package com.kgisl.resources.service;
+import java.util.List;
+import com.kgisl.resources.entity.*;
+
+
+
+public interface LevelService {
+
+    public Level create(Long id,String level);
+    public List<Level> get();
+   
+  
+ 
+
+}
