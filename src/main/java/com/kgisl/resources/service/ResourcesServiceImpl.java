63601d97939dbaf95b0085ced90fe2260e541f95
Ramyabalraj <57798910+Ramyabalraj@users.noreply.github.com>
2020-05-21 13:09:28 +0530

    Update ResourcesServiceImpl.java

--- src/main/java/com/kgisl/resources/service/ResourcesServiceImpl.java
+++ src/main/java/com/kgisl/resources/service/ResourcesServiceImpl.java
@@ -74,7 +74,7 @@ public class ResourcesServiceImpl implements ResourcesService {
     //  resourcesRepository.save(r6);
     //  resourcesRepository.save(r7);
     //  resourcesRepository.save(r8);
-     List<Resources> res = Arrays.asList(r,r1,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22);
+           List<Resources> res = Arrays.asList(r,r2,r3,r16,r4,r6,r7,r9,r11,r14,r15,r13,r17,r10,r18,r8,r19,r12,r21,r22,r1,r20);
      List<Resources> list = resourcesRepository.saveAll(res);
     //  List<Resources> list=  get();
      return list;
